bd8ba26e0cfcc1b06ced039a1a269aba0adc8b49
Nicolas Adrian Pessol Leal <54075371840@>
2025-08-08 11:31:32 -0300

    Livraria_DS19

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+#classe para o autor do livro
+class Autor(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    nationality = models.CharField(max_length=50, null=True)
+    bio = models.TextField()
